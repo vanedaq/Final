d30fe6d14c08acfdc6fcd236e8728e22b56736a7
vanedaq <90211990+vanedaq@users.noreply.github.com>
2025-09-13 17:15:02 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,31 @@
-// Service Worker muy simple de caché estático
-const CACHE = "finanzas-v1";
+const CACHE = 'of-cache-v10';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.app.js",
-  "./manifest.json"
+  './',
+  './index.html',
+  './style.app.css',
+  './app.app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener("activate", (e) => {
+self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
+  self.clients.claim();
 });
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', e=>{
+  const req=e.request;
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(req).then(res=>res || fetch(req).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+      return r;
+    }).catch(()=>res))
   );
 });
